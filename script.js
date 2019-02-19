b651f9b1698499b1b6631827f0b68cdb9be04690
dsweeney89 <47629301+dsweeney89@users.noreply.github.com>
2019-02-19 10:22:34 +0000

    Fixes

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(() => {
 
     $('#searchForm').on('submit', (e) => {
 
-        let searchText = ( $('#searchText').val() )
+        let searchText = ($('#searchText').val())
         getMovies(searchText);
         e.preventDefault();
         $(".middle").css("display", "block");
@@ -10,148 +10,106 @@ $(document).ready(() => {
 
     });
 
-   
-
-
 });
 
-
-
-function getMovies(searchText){
+function getMovies(searchText) {
     axios.get("http://www.omdbapi.com?s=" + searchText + "&apikey=145f5152")
-    .then((response) => {
-        console.log(response)
-        let movies = response.data.Search;
-        let output = "";
-        $.each(movies, (index, movie) => {
+        .then((response) => {
+            console.log(response)
+            let movies = response.data.Search;
+            let output = "";
+            $.each(movies, (index, movie) => {
+                
+                output += `
+            
+                    <div class="column is-one-third-desktop is-half-tablet is-full-mobile  is-centered">
+                    
+                        <div class="box  is-centered">
+                            <img class="lg-preview" src="${movie.Poster}">
+                            <h5 class="movie-title">${movie.Title} (${movie.Year})</h5>
+                            <button onclick="movieSelected('${movie.imdbID}')" class="button" id="#show-modal" href="#">Movie Info</button>
+                        </div>
+            
+                    </div>
+                    
+                        `;
+                                            })
 
-          
-            output += `
+                    $("#movies").html(output);
+
+                        })
+
+                .catch((err) => {
             
-            <div class="column is-one-third-desktop is-half-tablet is-full-mobile  is-centered">
-            
-            <div class="box  is-centered">
-                <img class="lg-preview" src="${movie.Poster}">
-                <h5 class="movie-title">${movie.Title} (${movie.Year})</h5>
-                <button onclick="movieSelected('${movie.imdbID}')" class="button" id="#show-modal" href="#">Movie Info</button>
-            </div>
-            
-            
-            </div>
-            
-            `;
+                        console.log(err)
         })
-
-        $("#movies").html(output);
-
-       
-
-    }) 
-
-    .catch((err) =>{
-        console.log(err)
-        
-    })
 }
 
 
+function movieSelected(id) {
 
-
-function movieSelected(id){
-    
-
- 
     axios.get("http://www.omdbapi.com?i=" + id + "&plot=full" + "&apikey=145f5152")
-    .then((response) => {
-        console.log(response)
-        
-        let movie = response.data;
-        let output = "";
+        .then((response) => {
+            console.log(response)
 
-        output += `
+            let movie = response.data;
+            let output = "";
 
-
-            
+            output += `
 
         <div class="modal">
             <div class="modal-background"></div>
                 <div class="modal-card">
-                    <header class="modal-card-head">
-                        <p class="modal-card-title">${movie.Title} (${movie.Year})</p>
-                        
-                            <button class="delete close-modal" onclick="closeModal()" aria-label="close"></button>
-                     </header>
-                                 <section class="modal-card-body">
+                    
+                <header class="modal-card-head">
+                    <p class="modal-card-title">${movie.Title} (${movie.Year})</p>
+                    <button class="delete close-modal" onclick="closeModal()" aria-label="close"></button>
+                </header>
+                                 
+                <section class="modal-card-body">
                                 
+                    <img class="sm-preview" src="${movie.Poster}">                            
+                    <img src="img/imdb.png" id="imdb">
+                    <span class="imdb-rating">Rating: ${movie.imdbRating}</span>
 
-                                <img class="sm-preview" src="${movie.Poster}">
-                             
-                                <img src="img/imdb.png" id="imdb">
-                                <span class="imdb-rating">Rating: ${movie.imdbRating}</span>
-
-                                <br>
-                                <br>
+                    <br>
+                    <br>
                                 
-                                <p>${movie.Plot}</p>
-                               
-                              
-                               
+                    <p>${movie.Plot}</p>
 
-                                </section>
-                                     <footer class="modal-card-foot">
+                </section>
                                      
-                               
-                                     
-                                     <p><span class="metadata">Country: </span>${movie.Country}</p>
-                                     <p><span class="metadata">Genre: </span>${movie.Genre}</p>
-                                     <p><span class="metadata">Metascore: </span>${movie.Metascore}</p>
-                                     <p><span class="metadata">Rated: </span>${movie.Rated}</p>
-                                     <p><span class="metadata">Runtime: </span>${movie.Runtime}</p>
-                                     <p><span class="metadata">Released: </span>${movie.Released}</p>
-                                    
-                                    
-                                     </footer>
+                <footer class="modal-card-foot">
+                                                         
+                    <p><span class="metadata">Country: </span>${movie.Country}</p>
+                    <p><span class="metadata">Genre: </span>${movie.Genre}</p>
+                    <p><span class="metadata">Metascore: </span>${movie.Metascore}</p>
+                    <p><span class="metadata">Rated: </span>${movie.Rated}</p>
+                    <p><span class="metadata">Runtime: </span>${movie.Runtime}</p>
+                    <p><span class="metadata">Released: </span>${movie.Released}</p>
+                                                 
+                </footer>
             </div>
       </div>
 
 
-
-
-
-
-
-
-
-
-
-
-
-
             `;
 
-           
+                    $(output).appendTo(".container-fluid")
 
-            console.log(this.data)
+        })
 
-        
-            
 
-            $(output).appendTo(".container-fluid")
+        .catch((err) => {
+            console.log(err)
 
-           
-    })
-
-    
-    .catch((err) =>{
-        console.log(err)
-        
-    })
+        })
 }
 
 
-function closeModal(){
+function closeModal() {
 
-     $(".modal").css("display", "none")
+    $(".modal").css("display", "none")
 
 }
 
